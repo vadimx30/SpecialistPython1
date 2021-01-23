f089a43c76e2d9455b5c0dcd588bd4286840c113
FToropov <54914310+FToropov@users.noreply.github.com>
2021-01-23 16:04:37 +0300

    Update m1_t4.py

--- Module1/practice/m1_t4.py
+++ Module1/practice/m1_t4.py
@@ -1,5 +1,5 @@
 rast = int(input('Расстояние (50 - 10000 км): '))
 rash = int(input('Расход (5-25 литров на 100 км): '))
 Benz = 48
-stoim = int(rast * rash / 100 * 48)
+stoim = int(rast * rash / 100 * Benz)
 print('Cтоимость поездки в рублях :', stoim)
