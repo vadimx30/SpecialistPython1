57cb0e2fa246ceac17459dcaaa88d8a349290f06
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-01-23 16:12:58 +0300

    Update m1_t2.py

--- Module1/practice/m1_t2.py
+++ Module1/practice/m1_t2.py
@@ -8,3 +8,14 @@ right =int(number[2])
 print ('Первая цифра:',left,
        '\nВторая цифра:',middle,
        '\nТретья цифра:',right)
+
+#через math
+import math
+number = int(input('Введите трехзначное число: '))
+left = int(number / 100)
+right = int(number % 10)
+middle = int(number - (left * 100))
+
+print ('Первая цифра:',left,
+       '\nВторая цифра:',middle // 10,
+       '\nТретья цифра:',right)
