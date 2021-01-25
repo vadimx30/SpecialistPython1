b8e2f4b755fbb85d0e323295b5baf9f75b2e5e15
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-01-25 20:57:28 +0300

    Update m1_t3.py

--- Module1/practice/m1_t3.py
+++ Module1/practice/m1_t3.py
@@ -1,14 +1,13 @@
 # Задача №3
 # Найти каждую цифру четырехзначного числа.
-
 import math
 number = int(input('Введите четырехзначное число: '))
-left = int(number / 1000)
-right1 = int(number % 100)
-middle = int(number - (left * 1000))
+left1 = int(number / 1000)
+left2 = int((number - left1 * 1000) // 100)
+right1 = int(number % 100 // 10)
 right2 = int(number % 10)
 
-print ('Первая цифра:',left,
-       '\nВторая цифра:',middle // 100,
-       '\nТретья цифра:',right1 // 10,
-       '\nЧетвертая цифра:',right2)
+print('Первая цифра:', left1, 
+    '\nВторая цифра:', left2, 
+    '\nТретья цифра:', right1, 
+    '\nЧетвертая цифра:', right2)
