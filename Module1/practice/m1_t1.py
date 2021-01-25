346aed7848cfb29626e9995bb508de8ba8975caa
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-01-25 20:54:45 +0300

    Update m1_t1.py

--- Module1/practice/m1_t1.py
+++ Module1/practice/m1_t1.py
@@ -2,14 +2,18 @@
 # Найти первую и последнюю цифры заданного трехзначного числа.
 
 number = str(input('Введите трехзначное число: '))
-left=int(number[0])
-right =int(number[2])
-print ('Первая цифра:',left,'\nТретья цифра:',right)
+left = int(number[0])
+right = int(number[2])
 
-#если через math
+print('Первая цифра:', left,
+    '\nТретья цифра:', right)
+
+
+#через math
 import math
 number = int(input('Введите трехзначное число: '))
-left=int(number/100)
-right=int(number%10)
-print ('Первая цифра:',left,
-       '\nТретья цифра:',right)
+left = int(number / 100)
+right = int(number % 10)
+
+print('Первая цифра:', left,
+    '\nТретья цифра:', right)
