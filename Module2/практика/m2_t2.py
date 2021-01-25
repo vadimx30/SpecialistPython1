4a2dcff40dab63e0475cd8d7b7c9fed2d2c657f4
Семён <77882441+SemLOl@users.noreply.github.com>
2021-01-25 10:03:56 +0300

    Update and rename Module2/practice/m2_t2.py to Module2/практика/m2_t2.py

---
+++ Module2/практика/m2_t2.py
@@ -0,0 +1,30 @@
+# a = int(input("число " ))
+# b = int(input("число " ))
+# c = int(input("число " ))
+# d = int(input("число " ))
+# e = int(input("число " ))
+# i = 0
+# k = 0
+# 
+# if a > 0:
+#     i = i + 1
+# elif a < 0:
+#     k = k + 1
+# if b > 0:
+#     i = i + 1
+# elif b < 0:
+#     k = k + 1
+# if c > 0:
+#     i = i + 1
+# elif c < 0:
+#     k = k + 1
+# if d > 0:
+#     i = i + 1
+# elif d < 0:
+#     k = k + 1
+# if e > 0:
+#     i = i + 1
+# elif e < 0:
+#     k = k + 1
+#     
+# print('количество положительных чисел:', i, 'количество отрицательных чисел:', k)  
