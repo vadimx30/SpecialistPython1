07c2b556bf99514f70d5c96e66743b3d858128b6
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-01-25 20:55:27 +0300

    Update m1_t2.py

--- Module1/practice/m1_t2.py
+++ Module1/practice/m1_t2.py
@@ -1,21 +1,21 @@
 # Задача №2
 # Найти каждую цифру трехзначного числа.
-
 number = str(input('Введите трехзначное число: '))
-left=int(number[0])
-middle=int(number[1])
-right =int(number[2])
-print ('Первая цифра:',left,
-       '\nВторая цифра:',middle,
-       '\nТретья цифра:',right)
+left = int(number[0])
+middle = int(number[1])
+right = int(number[2])
+print('Первая цифра:', left,
+    '\nВторая цифра:', middle,
+    '\nТретья цифра:', right)
+
 
 #через math
 import math
 number = int(input('Введите трехзначное число: '))
 left = int(number / 100)
+middle = int((number - left * 100) // 10)
 right = int(number % 10)
-middle = int(number - (left * 100))
 
-print ('Первая цифра:',left,
-       '\nВторая цифра:',middle // 10,
-       '\nТретья цифра:',right)
+print('Первая цифра:', left,
+    '\nВторая цифра:', middle,
+    '\nТретья цифра:', right)
