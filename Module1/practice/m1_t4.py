e5443adce67c7c0e7a8d77eb238f819c127a7f8a
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-01-25 21:38:21 +0300

    Update m1_t4.py
    От Матасова И.В.

--- Module1/practice/m1_t4.py
+++ Module1/practice/m1_t4.py
@@ -7,3 +7,10 @@
 
 Выход: стоимость поездки в рублях
 """
+import math
+distance = int(input('Введите расстояние (50 - 10000 км): '))
+fuel_consumption100 = int(input('Введите расход топлива (5 - 25 литров/100км): '))
+fuel_consumption1 = float(fuel_consumption100/100)
+fuel_price = int(48)
+trip_cost = float(round((distance * fuel_consumption1 * fuel_price), 2))
+print('Стоимость поездки составляет ', trip_cost, 'рублей')
