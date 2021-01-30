3db22ff6d10cdda0c569daf8e2857340efca7de6
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-01-30 17:28:27 +0300

    Update m3_t4.py

--- Module3/practice/m3_t4.py
+++ Module3/practice/m3_t4.py
@@ -5,3 +5,12 @@
 In: 'antuh1ouou21au3'
 Out: 1213
 """
+
+
+string = input('Enter string: ')
+result = [item for item in string if item >= '0' and item <='9']
+if result:
+    for i in result:
+        print(i,end='')
+else:
+    print('No numeric')
