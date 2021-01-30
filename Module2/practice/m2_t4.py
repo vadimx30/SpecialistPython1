df8bd8e6b903c6ff4766fdaf43afc01b9a121b84
Luka2003-tch <77882552+Luka2003-tch@users.noreply.github.com>
2021-01-30 12:55:03 +0300

    Update m2_t4.py

--- Module2/practice/m2_t4.py
+++ Module2/practice/m2_t4.py
@@ -1,2 +1,35 @@
 # Задача №4
 # В заданной строке найти количество вхождений символа 'a'
+While Условие:
+  Тело цикла (инструкция)
+  изменение условия 
+  
+  
+a, b = 5, 15
+while a < b:
+  <blick of instructions>
+  print ('a =', a)
+  a += 5
+  if a ++ 10: ##Переход на новый цикл, пропуская выполнение print
+    print('Переход на новую строку')
+    continues
+ if a == 20:
+     break
+    print ('new a: {}'.format(a)
+# 
+print(' Total valuse a: {}'.format(a))
+### 
+for item in range(15):
+  print(item)
+print('-' * 15)
+for item in range (5, 15):
+           print('четные: ', item)
+print('-' * 15)
+for item in range(15, 5, 0):
+           print(item)
+# # #
+greet 'Find a'
+for i in range(3, len(greet)): ## for i in range(12)
+    print('Value of i: ', i)
+    if greet[i] == 'w':
+        print(f'index of w is {i}')
