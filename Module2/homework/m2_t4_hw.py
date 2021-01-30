cad71fd78a514547f78ba73fbc7ac83a492c8c9d
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-01-30 13:15:51 +0300

    Update m2_t4_hw.py

--- Module2/homework/m2_t4_hw.py
+++ Module2/homework/m2_t4_hw.py
@@ -3,3 +3,21 @@
 # Выход: количество вхождений символа в строку.
 # Вводим строку до тех пор, пока длина строки
 # меньше 5 символов.
+
+lens = 0
+while lens <5:
+    string = input('Enter string ')
+    lens = len(string)
+    if lens <5: print('Need 5 chars minimum!')
+ 
+lens = 0
+while not lens == 1:
+    char = input('enter char ')
+    lens = len(char)
+    if not lens == 1: print('Need a char!')
+
+qq = 0
+for i in char:
+    if i == char:
+        qq += 1
+print(f'Quantity {char} in {string} = {qq}')
