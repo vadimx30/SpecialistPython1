e88cb0243e9ebb67eef1f0a674463bbd69b0eca8
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-01-30 12:46:51 +0300

    Update m2_t4.py

--- Module2/practice/m2_t4.py
+++ Module2/practice/m2_t4.py
@@ -1,8 +1,8 @@
 # Задача №4
 # В заданной строке найти количество вхождений символа 'a'
 string = input('Enter string: ')
-Q = 0
+q = 0
 for i in string:
     if i == 'a':
-        Q+=1
-print(f'Quantity a in {string} = {Q}')
+        q + = 1
+print(f'Quantity a in {string} = {q}')
