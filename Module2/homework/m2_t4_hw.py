dbb11ef2cf940e61539e69fa43ef84bfb68dc46d
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-01-30 13:40:32 +0300

    Update m2_t4_hw.py
    От Матасова И.В.
    Есть ли способ не повторять блок счетчика "for" в данном случае?

--- Module2/homework/m2_t4_hw.py
+++ Module2/homework/m2_t4_hw.py
@@ -3,3 +3,19 @@
 # Выход: количество вхождений символа в строку.
 # Вводим строку до тех пор, пока длина строки
 # меньше 5 символов.
+
+letter = input('Введите символ: ')
+text = input('Введите текст: ')
+count = 0
+
+while len(text) < 5:
+        for i in text:
+            if i == letter:
+                count += 1
+        text = input('Введите текст: ')
+else:
+        for i in text:
+            if i == letter:
+                count += 1
+        
+print(f'Накопленное количество вхождений символа "{letter}" в строку: {count}')
