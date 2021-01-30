719bb1099f75a121ebd72479ad3769595b667752
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-01-30 18:04:49 +0300

    Update m3_t2_hw.py

--- Module3/homework/m3_t2_hw.py
+++ Module3/homework/m3_t2_hw.py
@@ -11,3 +11,25 @@ for _ in range(10):
 from random import randint
 res = [randint(1, 100) for _ in range(10)]
 """
+
+
+from random import randint
+N = int(input('Enter N:'))
+res = [randint(1, 100) for _ in range(N)]
+ch1 = ''
+ch2 = ''
+for i in res:
+    if ch1 == '': #First
+        ch1 = i
+    else:
+        if i < ch1:
+            ch2 = ch1
+            ch1 = i
+        else:
+            if ch2 == '':
+                ch2 = i
+            else:
+                if i < ch2:
+                    ch2 = i
+print(res)
+print(f'Two minimum is {ch1} and {ch2}')
