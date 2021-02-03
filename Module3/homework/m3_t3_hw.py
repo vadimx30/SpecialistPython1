006d2fef2a6ff888bb4ca76ffa4ccb440d128040
dmitrylipat <77882257+dmitrylipat@users.noreply.github.com>
2021-02-03 11:39:14 +0300

    Update m3_t3_hw.py

--- Module3/homework/m3_t3_hw.py
+++ Module3/homework/m3_t3_hw.py
@@ -5,3 +5,23 @@ p1*p2*p3*....*pn, где pi - цифра. Вычислите значение в
 In: 5*3*2
 Out: 30
 """
+
+a = [i for i in input('Введите последовательность типа a*b*c не более 9 символов')]
+while len(a) >= 9:
+    a = [i for i in input('Введите последовательность типа a*b*c не более 9 символов')]
+nach = a.copy()
+a.append('*')
+b = ''
+c = []
+for i in range(len(a)):
+    if a[i] != '*':
+        b += a[i]
+    else:
+      c.append(b)
+      b = ''
+for k in range(len(c)):
+    c[k] = int(c[k])
+result = 1
+for g in range(len(c)):
+    result *= c[g]
+print('Значение выражения', *nach, '=', result)
