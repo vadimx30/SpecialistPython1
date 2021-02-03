2e04cb461b9a24208cb73123579ee23565c1d4ed
dmitrylipat <77882257+dmitrylipat@users.noreply.github.com>
2021-02-03 11:05:36 +0300

    Update m3_t2_hw.py

--- Module3/homework/m3_t2_hw.py
+++ Module3/homework/m3_t2_hw.py
@@ -11,3 +11,12 @@ for _ in range(10):
 from random import randint
 res = [randint(1, 100) for _ in range(10)]
 """
+
+from random import randint
+res = [randint(1, 100) for _ in range(10)]
+print(f'Введеные рандомные числа: {res}')
+res.sort()
+a = []
+a.append(res[0])
+a.append(res[1])
+print('Два наименьших числа: ', *a)
