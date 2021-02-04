6a85b80fd20ea8d714dfb00d85f9a078db0acf8c
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-02-05 00:06:10 +0300

    Update m3_t4.py
    От Матасова И.В.

--- Module3/practice/m3_t4.py
+++ Module3/practice/m3_t4.py
@@ -5,3 +5,16 @@
 In: 'antuh1ouou21au3'
 Out: 1213
 """
+
+text = input('Введите текст: ')
+lst = [_ for _ in text]
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+text_numbers=list()
+
+for _ in lst:
+    if _ in DIGITS:
+        text_numbers.append(_)
+if text_numbers == []:
+    print('Чисел нет')
+else:
+    print(f'Введенные числа: {"".join(text_numbers)}')
