3adf72f2d4372ee61634d950b4b1a3c905385bf9
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-02-06 00:16:38 +0300

    Update m3_t1_hw.py

--- Module3/homework/m3_t1_hw.py
+++ Module3/homework/m3_t1_hw.py
@@ -18,3 +18,29 @@ In: 3 8 8 11  Out: WEAKLY ASCENDING
 In: 2 -1 7    Out: RANDOM
 In: 5 5 5 5   Out: CONSTANT
 """
+
+text = input('Введите последовательность целых чисел: ').split()
+import sys
+try:
+    sequence = [int(_) for _ in text]
+except ValueError:
+    print('Введите числа')
+    sys.exit()
+
+if all(sequence[i] == sequence[i+1] for i in range(len(sequence)-1)):
+    print(f'In: {" ".join(text)} \nOut: CONSTANT')
+
+elif all(sequence[i] <= sequence[i+1] for i in range(len(sequence)-1)):
+    if all(sequence[i] < sequence[i+1] for i in range(len(sequence)-1)):
+        print(f'In: {" ".join(text)} \nOut: ASCENDING')
+    else:
+        print(f'In: {" ".join(text)} \nOut: WEAKLY ASCENDING')
+
+elif all(sequence[i] >= sequence[i+1] for i in range(len(sequence)-1)):
+    if all(sequence[i] > sequence[i+1] for i in range(len(sequence)-1)):
+        print(f'In: {" ".join(text)} \nOut: DESCENDING')
+    else:
+        print(f'In: {" ".join(text)} \nOut: WEAKLY DESCENDING')
+
+else:
+    print(f'In: {" ".join(text)} \nOut: RANDOM')
