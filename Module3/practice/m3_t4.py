73eef5e655ba63d3d2f2cccdcc184c235871486c
any-shmidt <77882706+any-shmidt@users.noreply.github.com>
2021-02-05 19:09:41 +0300

    Update m3_t4.py

--- Module3/practice/m3_t4.py
+++ Module3/practice/m3_t4.py
@@ -5,3 +5,16 @@
 In: 'antuh1ouou21au3'
 Out: 1213
 """
+text = (input('enter:'))
+list1 = []
+list2 = []
+for i in text:
+    list1.append(i)
+for i in list1:
+    if (i.isdigit()):
+        list2.append(i)
+if list2 != []:
+    a = ''.join(list2)
+    print(a)
+else:
+    print('Цифр нет')
