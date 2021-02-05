c4256bcec958c45723256e08da42030f94e88369
Luka2003-tch <77882552+Luka2003-tch@users.noreply.github.com>
2021-02-05 20:41:03 +0300

    Update m3_t3.py

--- Module3/practice/m3_t3.py
+++ Module3/practice/m3_t3.py
@@ -14,3 +14,17 @@ t: 1
 n: 1
 1: 1
 """
+int main () 
+  while (printf 'string')
+         int cur = 0 
+         int max = 13
+if(!latletter(str[i])){s=1; i+=1; continue;}
+         H=1 , e = 1 , l = 2 o =2;
+         , = 1 , p = 1 , y =1 , t =1
+         n = 1 , 1 = 1
+   5 + 8 = 13
+
+   printf("%d\n", max);
+    return 0;
+
+         
