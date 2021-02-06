84ff1a0be0087999e2035d49a883c3fab8b635e5
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-02-06 16:07:20 +0300

    Update m4_t1.py

--- Module4/practice/m4_t1.py
+++ Module4/practice/m4_t1.py
@@ -12,3 +12,35 @@
 Третья функция принимает в качестве аргументов площадь и стоимость одного квадратного метра земли.
 Возвращает итоговую сумму.
 """
+
+def param(a: float, b: float) -> tuple:
+    '''определение площади'''
+    perimetr = a * 2 + b * 2
+    s = a * b
+    return perimetr, s
+
+def zabor(perimetr: float, zabor_price: float) -> float:
+    '''определение стоимости забора'''
+    zabor_cost = perimetr * zabor_price
+    return zabor_cost
+
+def acrage(s: float, acrage_price: float) -> float:
+    '''определение стоимости земельного участка'''
+    acrage_cost = s * acrage_price
+    return acrage_cost
+
+def main():
+    a = float(input('Длина: '))
+    b = float(input('Ширина: '))
+    zabor_price = float(input('Цена погонного метра забора: '))
+    acrage_price = float(input('Цена квадратного метра земли: '))
+    
+    perimetr, s = param(a, b)
+    zabor_cost = zabor(perimetr, zabor_price)
+    acrage_cost = acrage(s, acrage_price)
+    print('Параметры (периметр и площадь):', *(param(a, b)))
+    print('Стоимость забора:', zabor_cost)
+    print('Стоимость участка:', acrage_cost)
+
+if __name__ == '__main__':
+    main()
