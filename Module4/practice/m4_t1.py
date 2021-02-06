3fa9f36d6f9d93d29b58eb5b504bf722a46c3ea7
dmitrylipat <77882257+dmitrylipat@users.noreply.github.com>
2021-02-06 15:26:19 +0300

    Update m4_t1.py

--- Module4/practice/m4_t1.py
+++ Module4/practice/m4_t1.py
@@ -12,3 +12,30 @@
 Третья функция принимает в качестве аргументов площадь и стоимость одного квадратного метра земли.
 Возвращает итоговую сумму.
 """
+
+import func
+a = float(input('Введите длину участка: '))
+b = float(input('Введите ширину участка: '))
+spm = float(input('Введите стоимость погонного метра забора: '))
+skm = float(input('Введите стоимость квадратного метра земли: '))
+p, s = func.param(a, b)
+print('Стоимость забора: ', func.summzabor(p, spm))
+print('Стоимость земли: ', func.summearth(s, skm))
+
+# Модуль с функциями
+def param(a: float, b: float) -> tuple:
+    perimetr = (a + b) * 2
+    perimetr = round(perimetr, 2)
+    ploshad = a * b
+    ploshad = round(ploshad, 2)
+    return perimetr, ploshad
+
+def summzabor(p: float, spm: float) -> float:
+    zabor = p * spm
+    zabor = round(zabor, 2)
+    return zabor
+
+def summearth(s: float, skm: float) -> float:
+    earth = s * skm
+    earth = round(earth, 2)
+    return earth
