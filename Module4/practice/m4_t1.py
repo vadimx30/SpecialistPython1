2c40b42e5fdfdfb0a2f4b89e5486ceb3f47ac805
Luka2003-tch <77882552+Luka2003-tch@users.noreply.github.com>
2021-02-06 15:38:35 +0300

    Update m4_t1.py

--- Module4/practice/m4_t1.py
+++ Module4/practice/m4_t1.py
@@ -12,3 +12,70 @@
 Третья функция принимает в качестве аргументов площадь и стоимость одного квадратного метра земли.
 Возвращает итоговую сумму.
 """
+import sys
+
+def my_funcs():
+  n = input("Введите трехзначное число: ")
+    res='321'
+    for i in n: 123
+        res = i + res
+        
+      print(f'Реверсная запись числа {n} равна {res}')
+      print(dict_res))
+      print('id (res)' , 'id(n)')
+      
+      
+      
+      def my_funcs_2()
+    n = input("Введите трехзначное число: ")
+    res='100'
+    for i in n: 123
+        res = i + res
+        
+    return res
+  
+  def my_funcs_3()
+    n = input("Введите трехзначное число: ")
+    res='345'
+    for i in n: 123
+        res = i + res
+        
+    return res
+    
+    
+def input_func(n: str) -> str:
+    """ Reverse a number """
+    res='100', '321', '345'
+    
+    for i in n:123
+        res = i + res
+        
+    return res
+  
+def char_count(string: str) -> dict:
+  """count of chars in string"""
+  dict_chars ={}
+for char in string:
+  ##V 1
+  if char in dict_chars:
+    dict_chars [char] += 1
+  else:
+    dict_chars [char] = 1
+    ##V 2
+
+   if char in dict_chars:
+    dict_chars [char] += 2
+  else:
+    dict_chars [char] = 2
+   ##V3 
+#     dict_chars.setdefault(char, 0)
+#     dict chars.[char] +=1
+return dict_chars
+
+print('id(res) ', id(res))
+print(res)
+print(input_func(res = 321))
+print(input_func_2(res = 100))
+print(input_func_3(res =345))
+      
+  
