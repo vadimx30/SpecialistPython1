1cbeae9b8db72f39ed66e38e65c60fff2ced43cf
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-02-06 15:20:15 +0300

    Update m4_t1.py

--- Module4/practice/m4_t1.py
+++ Module4/practice/m4_t1.py
@@ -12,3 +12,27 @@
 Третья функция принимает в качестве аргументов площадь и стоимость одного квадратного метра земли.
 Возвращает итоговую сумму.
 """
+#mymodule.py
+def getPS(a:float,b:float):
+    p = a+a+b+b
+    s= a*b
+    return p,s
+
+def getSumZabor(p:float,c:float):
+    return p*c
+
+def getSumEarth(s:float,c:float):
+    return s*c
+#end mymodule.py
+
+import mymodule
+a = float(input('Enter a: '))
+b = float(input('Enter b: '))
+c = float(input('Enter c of zabor: '))
+cc = float(input('Enter c of earth: '))
+
+p,s = mymodule.getPS(a,b)
+cz = mymodule.getSumZabor(p,c)
+ce = mymodule.getSumEarth(s,cc)
+
+print(f'perim = {p}, square = {s}, Zabor = {cz}, Earth = {cz}')
