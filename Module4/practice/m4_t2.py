3a5f2b9aa0e12a65dd829637947e58b80619ae44
dmitrylipat <77882257+dmitrylipat@users.noreply.github.com>
2021-02-06 15:52:56 +0300

    Update m4_t2.py

--- Module4/practice/m4_t2.py
+++ Module4/practice/m4_t2.py
@@ -5,3 +5,23 @@
 2) Ai = Bi
 3) Ai < Bi
 '''
+
+a  = tuple(map(int, input('Введите первый котреж чисел: ').split()))
+b = tuple(map(int, input('Введите второй котреж чисел: ').split()))
+if len(a) != len(b):
+    print('количество символов разное')
+    while len(b) != len(a):
+        b = tuple(input(f'Введите второй кортеж из {len(a)} чисел: '))
+bol = 0
+men = 0
+rav = 0
+for i in range(len(a)):
+    if a[i]  > b[i]:
+        bol += 1
+    elif a[i] < b[i]:
+        men += 1
+    else:
+        rav += 1
+print(f'Ai > Bi:  {bol}')
+print(f'Ai = Bi: {rav}')
+print(f'Ai < Bi: {men}')
