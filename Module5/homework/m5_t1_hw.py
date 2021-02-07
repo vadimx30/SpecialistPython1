7fa77946cee6d77214993ed0263bd62cfd8a3367
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-02-07 23:46:15 +0300

    Update m5_t1_hw.py
    Использовал методы str

--- Module5/homework/m5_t1_hw.py
+++ Module5/homework/m5_t1_hw.py
@@ -16,3 +16,37 @@
 хороший пароль -> o58anuahaunH!
 плохой пароль -> saucacAusacu8  -> нет
 '''
+
+
+a = 0
+while a < 5:
+    a += 1
+    d = {'Up':0,'Low':0,'Spec':0,'Num':0}
+    string = input('Enter password: ')
+    for char in string:
+        if str.isdigit(char):
+            d['Num'] += 1
+        if str.islower(char):
+            d['Low'] += 1
+        if str.isupper(char):
+            d['Up'] += 1
+        if not str.isalnum(char):
+            d['Spec'] += 1
+    a += 1
+    if len(string) > 7 and len(string) <16 and not 0 in d.values():
+        print(f'Password <{string}> is right')
+        break
+    error_str = ''
+    if len(string) < 8 or len(string) > 15:
+        error_str = error_str +'Need 8-15 simbols. '
+    if d['Up'] == 0:
+         error_str = error_str +'Need one or more upper simbols. '
+    if d['Low'] == 0:
+         error_str = error_str +'Need one or more lower simbols. '
+    if d['Spec'] == 0:
+         error_str = error_str +'Need one or more spec simbols. '
+    if d['Num'] == 0:
+         error_str = error_str +'Need one or more numeric. '
+    print(f'Try {a}: Password <{string}> incorrect. {error_str}Try again')
+else:
+    print('!!! No more try. Access denied !!!')
