3e44bb0ce4375aa70162f2750cdba8aa66cf42b7
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-02-07 22:53:13 +0300

    Update m5_t3.py

--- Module5/practice/m5_t3.py
+++ Module5/practice/m5_t3.py
@@ -8,3 +8,12 @@ a4 b4 c4 d4
 a3 b3 c3 d3
 a2 b2 c2 d2
 a1 b1 c1 d1
+
+
+dlsh = int(input('Enter chessmath value: '))
+for a  in range(dlsh):
+    for b in range(dlsh):
+        x = dlsh - a
+        y = chr(97+ b)
+        print(f'{y}{x}',end = ' ')
+    print()
