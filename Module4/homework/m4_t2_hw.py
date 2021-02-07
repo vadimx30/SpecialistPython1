1e4d679d4e678a51797f32c62d72ba47043ac1b2
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-02-07 22:24:07 +0300

    Update m4_t2_hw.py
    Чуть поменял код. Так больше соответствует заданию

--- Module4/homework/m4_t2_hw.py
+++ Module4/homework/m4_t2_hw.py
@@ -6,7 +6,6 @@ x1, y2 = input().split()
 Вторая функция должна определить, является ли треугольник прямоугольным.
 """
 
-
 def get_storony(xy1,xy2,xy3):
     ax = xy2[0]-xy1[0]
     ay = xy2[1]-xy1[1]
@@ -25,12 +24,17 @@ def proove(a,b,c):
     else:
         return False
 
-def get_square(a,b,c):
+def get_square(aa,bb,cc,method = False):#Tho
+    if method == False:
+        a,b,c = get_storony([aa[0],aa[1]],[bb[0],bb[1]],[cc[0],cc[1]])
+    else:
+        a,b,c = aa,bb,cc
     p = 0.5*(a+b+c)
     return round((p*(p-a)*(p-b)*(p-c))**0.5,2)
 
 def chk_right(a,b,c):
-    s = get_square(a,b,c)
+    a,b,c = get_storony([x1,y1],[x2,y2],[x3,y3])
+    s = get_square(a,b,c,True)
     if (proove(1/2*a*b,s,0.01) or
         proove(1/2*a*c,s,0.01) or
         proove(1/2*b*a,s,0.01) or
@@ -41,11 +45,10 @@ def chk_right(a,b,c):
     else:
         return False
 
-x1,y1 = map(int,input('Enter point 1:(x y) ').split())
+x1,y1 = map(int,input('Enter point 1:(x y) ').split())    
 x2,y2 = map(int,input('Enter point 2:(x y) ').split())
 x3,y3 = map(int,input('Enter point 3:(x y) ').split())
-a,b,c = get_storony([x1,y1],[x2,y2],[x3,y3])
-s = get_square(a,b,c)
-rg = chk_right(a,b,c)
-print(f'Storony = {a}, {b}, {c}; Square = {s}, This is right: {rg}')
+s = get_square([x1,y1],[x2,y2],[x3,y3])
+rg = chk_right([x1,y1],[x2,y2],[x3,y3])
+print(f'Square = {s}, This is right: {rg}')
 
