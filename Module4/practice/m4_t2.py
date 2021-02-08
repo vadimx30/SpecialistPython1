8d351df5cd0d743b4aaeafece4a656742338301b
dmitrylipat <77882257+dmitrylipat@users.noreply.github.com>
2021-02-08 09:37:37 +0300

    Update m4_t2.py

--- Module4/practice/m4_t2.py
+++ Module4/practice/m4_t2.py
@@ -6,22 +6,29 @@
 3) Ai < Bi
 '''
 
+import func
 a  = tuple(map(int, input('Введите первый котреж чисел: ').split()))
 b = tuple(map(int, input('Введите второй котреж чисел: ').split()))
 if len(a) != len(b):
     print('количество символов разное')
     while len(b) != len(a):
         b = tuple(input(f'Введите второй кортеж из {len(a)} чисел: '))
-bol = 0
-men = 0
-rav = 0
-for i in range(len(a)):
-    if a[i]  > b[i]:
-        bol += 1
-    elif a[i] < b[i]:
-        men += 1
-    else:
-        rav += 1
-print(f'Ai > Bi:  {bol}')
-print(f'Ai = Bi: {rav}')
-print(f'Ai < Bi: {men}')
+bl, m, r = func.poisk(a, b)
+print(f'Ai > Bi:  {bl}')
+print(f'Ai = Bi: {r}')
+print(f'Ai < Bi: {m}')
+
+#func
+def poisk(a: tuple, b:tuple) -> tuple:
+    """poisk neravenstv dvuh kortezhey"""
+    bol = 0
+    men = 0
+    rav = 0
+    for i in range(len(a)):
+        if a[i]  > b[i]:
+            bol += 1
+        elif a[i] < b[i]:
+            men += 1
+        else:
+            rav += 1
+    return bol, men, rav
