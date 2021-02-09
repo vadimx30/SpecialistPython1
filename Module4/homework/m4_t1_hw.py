2123ff4f5f67f4c058c101cef7b53ad50747ff21
Luka2003-tch <77882552+Luka2003-tch@users.noreply.github.com>
2021-02-09 20:20:49 +0300

    Update m4_t1_hw.py

--- Module4/homework/m4_t1_hw.py
+++ Module4/homework/m4_t1_hw.py
@@ -11,4 +11,65 @@ Out: 1 бутылка
     
 In: 22
 Out: 22 бутылки
-"""
+
+
+
+def My_fyrst_func(parameters):
+    statement(s)
+      if x > 0:
+      return 5
+else:
+    if x < 0 
+    return 5
+def greet():
+    print("In"):
+    In = print( 0 , or , 5)
+print(first)
+# In: 5
+def greet():
+    print("Out"):
+    Out = print(0 , or , 5)
+print(first)
+# Out: 5
+#Тем самым мы найдем ответ на первую созданную нами функцию
+  def My_second_fun(parametr):
+  statement(s):
+print ("In x > 0 , In x < 0")
+    return 1 
+print (" Out x > 1 , Out x < 0)
+    return  1
+else:
+first, second = give_me_two_fives()
+    print (first)
+    in = 1
+    print (second)
+    Out = 1
+    result 1
+ #Вторая созданная функция 
+ def My_fyrst_func(parameters):
+    statement(s)
+      if x > 0:
+      return 5
+else:
+    if x < 0 
+    return 22
+def greet():
+    print("In"):
+    In = print( 0 , or , 22)
+print(first)
+# In: 22
+def greet():
+    print("Out"):
+    Out = print(0 , or , 22)
+print(first)
+# Out: 22
+   print ("result 0 , or , 44")
+   return 0
+ 
+
+    
+ 
+      
+      
+ 
+    
