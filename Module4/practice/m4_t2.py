a12c1140692cdb78b7235df88540644bbe1f6c33
FToropov <54914310+FToropov@users.noreply.github.com>
2021-02-09 14:48:45 -0300

    Update m4_t2.py

--- Module4/practice/m4_t2.py
+++ Module4/practice/m4_t2.py
@@ -5,3 +5,18 @@
 2) Ai = Bi
 3) Ai < Bi
 '''
+a = (1, 2, 1)
+b = (1, 1, 3)
+perem_1 = 0
+perem_2 = 0
+perem_3 = 0
+
+for i in range(len(a)):
+    if a[i] > b[i]:
+        perem_1 += 1
+    elif a[i] == b[i]:
+        perem_2 += 1
+    elif a[i] < b[i]:
+        perem_3 += 1
+    
+print(perem_1, perem_2, perem_3)
