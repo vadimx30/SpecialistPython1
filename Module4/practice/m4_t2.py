cfa7a1ca2d839cfe28d4f543c75e718914b08bbc
any-shmidt <77882706+any-shmidt@users.noreply.github.com>
2021-02-12 21:11:43 +0300

    Update m4_t2.py

--- Module4/practice/m4_t2.py
+++ Module4/practice/m4_t2.py
@@ -5,3 +5,18 @@
 2) Ai = Bi
 3) Ai < Bi
 '''
+a = input('enter:').split()
+b = input('enter:').split()
+c = 0
+d = 0
+e = 0
+for i in range(len(a)):
+    if a[i] > b[i]:
+        c +=1
+    if a[i] == b[i]:
+        d +=1
+    if a[i] < b[i]:
+        e +=1
+print(c)
+print(d)
+print(e)
