1b8b70e02e4ab075b23a1c569d91f146c90fd907
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-02-13 15:42:44 +0300

    Update m6_t1.py

--- Module6/practice/m6_t1.py
+++ Module6/practice/m6_t1.py
@@ -1,3 +1,15 @@
 """
 Написать программу для подсчета размера всех .py файлов в текущей папке.
 """
+
+import os
+import glob
+
+files = glob.glob('*.py')
+print(files, type(files))
+summ = 0
+for i in files:
+    summ += os.path.getsize(i)
+    
+print(f' В текущей папке {len(files)} файлов,'
+      f'общим объемом {summ//1024}Кб')
