74db06662711ada034a01865eb8bd3c5f40155e1
FToropov <54914310+FToropov@users.noreply.github.com>
2021-02-13 15:47:33 +0300

    Update m6_t2.py

--- Module6/practice/m6_t2.py
+++ Module6/practice/m6_t2.py
@@ -2,3 +2,16 @@
 Написать программу, которая находит все файлы заданного расширения и
 переносит их в подпапку backup, которую нужно создать.
 """
+import os
+import glob
+import shutil
+
+os.mkdir('backup')
+ras = input('Введите раширение: ')
+# print(ras)
+from pprint import pprint
+a = glob.glob(f'*{ras}')
+# print(a)
+for i in a:
+    shutil.copy(i, "backup")
+    os.remove(i)
