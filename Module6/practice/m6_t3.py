e712c50f52d2ebe4e895ff2d3570642218fac41b
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-02-13 15:39:32 +0300

    Update m6_t3.py

--- Module6/practice/m6_t3.py
+++ Module6/practice/m6_t3.py
@@ -3,3 +3,24 @@
  из 20 элементов и сохранить в файл.
  После этого, считать числа из файла и посчитать среднее арифметическое.
  """
+import random
+f = 'spisok.rnd'
+file = open(f,'w')
+for i in range(20):
+    char = str(random.randint(1, 1000))+'\n'
+    file.write(char)
+file.close()
+
+
+a,ar = 0,0
+with open(f) as file:
+    for smv in file.readlines():
+        j = smv[0:len(smv)-1]
+        if str.isdigit(j):
+            ar += int(j)
+            a += 1
+if not(a == 0):
+    srd = ar/a
+else:
+    srd = 0
+print(f'{a} Srednee = {srd}')
