f42c750d8ddb440da3b119dcda982577bdde7ee1
dmitrylipat <77882257+dmitrylipat@users.noreply.github.com>
2021-02-13 15:02:08 +0300

    Update m6_t1.py

--- Module6/practice/m6_t1.py
+++ Module6/practice/m6_t1.py
@@ -1,3 +1,10 @@
 """
 Написать программу для подсчета размера всех .py файлов в текущей папке.
 """
+
+import os
+a = os.listdir(os.curdir)
+b = 0
+for i in a:
+    b += os.path.getsize(i)
+print(f'{b} KB')
