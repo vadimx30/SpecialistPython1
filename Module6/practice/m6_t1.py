7371435a78f684f3a808a2c6fc19d148710cedb5
Luka2003-tch <77882552+Luka2003-tch@users.noreply.github.com>
2021-02-13 15:46:20 +0300

    Update m6_t1.py

--- Module6/practice/m6_t1.py
+++ Module6/practice/m6_t1.py
@@ -1,3 +1,13 @@
 """
 Написать программу для подсчета размера всех .py файлов в текущей папке.
 """
+import os
+import glob
+path = input ('Write path -> ')    
+os.chdir(path)                            
+dir_work = os.listdir(path)        
+a = 1
+b = 2
+summ = 3
+for file in glob.glob("*.txt"):
+    print(file)
