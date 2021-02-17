5de7eaf62d69cb3e294d33ea719f1489a27e405a
FToropov <54914310+FToropov@users.noreply.github.com>
2021-02-17 12:47:50 -0300

    Update m6_t3.py

--- Module6/practice/m6_t3.py
+++ Module6/practice/m6_t3.py
@@ -3,3 +3,15 @@
  из 20 элементов и сохранить в файл.
  После этого, считать числа из файла и посчитать среднее арифметическое.
  """
+import statistics
+from random import randint
+x = 20
+with open('m6_t3.txt', 'w') as f:
+    for _ in range(x):
+        f.write(str(randint(1, 1000)) + '\n')
+
+with open('m6_t3.txt', 'r') as f:
+    l = [int(line.strip()) for line in f]
+
+m = statistics.mean(l)
+print(m)
