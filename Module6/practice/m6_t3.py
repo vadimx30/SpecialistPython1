bc1ee2bd19e432260a9224b174c5dab19ab578be
dmitrylipat <77882257+dmitrylipat@users.noreply.github.com>
2021-02-19 10:02:20 +0300

    Update m6_t3.py

--- Module6/practice/m6_t3.py
+++ Module6/practice/m6_t3.py
@@ -3,3 +3,20 @@
  из 20 элементов и сохранить в файл.
  После этого, считать числа из файла и посчитать среднее арифметическое.
  """
+
+from random import randint
+elem = [randint(1, 1000) for _ in range(20)]
+resultwrite = open('result.txt', 'w')
+for i in elem:
+    i = str(i) + '\n'
+    resultwrite.write(i)
+resultwrite.close()
+srarifm = 0
+resultread = open('result.txt', 'r')
+summ = [k for k in resultread.readlines()]
+for m in summ:
+    m = int(m)
+    srarifm += m
+resultread.close()
+srarifm = srarifm / 20
+print(f'Среднее арифметическое = {srarifm}')
