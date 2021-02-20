aea95b79455fce125e5ac654e63cfc40324663da
any-shmidt <77882706+any-shmidt@users.noreply.github.com>
2021-02-20 16:23:31 +0300

    Update m7_t1_hw.py

--- Module7/homework/m7_t1_hw.py
+++ Module7/homework/m7_t1_hw.py
@@ -10,3 +10,79 @@
 Эти данные не могут быть пустыми.
 Проверить правильность заполнения полей, используя исключения.
 """
+while True:
+    name = input('Наименование товара:')
+    if name.isalnum():
+        break
+    if not(name.isalnum()):
+        name = input('Наименование товара:')
+        
+try:
+    quantity = int(input('Количество:'))
+except(ValueError):
+    quantity = int(input('Количество:'))
+    
+while True:
+    surname = input('Фамилия получателя:')
+    if surname.isalpha():
+        break
+    if not(surname.isalpha()):
+        surname = input('Фамилия получателя:')
+        
+while True:
+    try:
+        phone = int(input('Контактный телефон (10 цифр):'))
+        a = str(phone)
+        if len(a) == 10:
+            break
+        if len(a) < 10 and len(a) > 10:
+            phone = int(input('Контактный телефон (10 цифр):'))
+    except(ValueError):
+        print('Not a number')
+        
+print('Адрес')
+while True:
+    try:
+        index = int(input('Индекс (6 цифр):'))
+        b = str(index)
+        if len(b) == 6:
+            break
+        if len(b) < 6 and len(b) > 6:
+            index = int(input('Индекс (6 цифр):'))
+    except(ValueError):
+        print('Not a number')
+        
+while True:
+    city = input('Город:')
+    if city.isalpha():
+        break
+    if not(city.isalpha()):
+        city = input('Город:')
+        
+while True:
+    street = input('Улица:')
+    if street.isalpha():
+        break
+    else:
+        street = input('Улица:')
+
+while True:
+    try:
+        haus = int(input('Номер дома:'))
+        break
+    except(ValueError):
+        print('Not a number')
+        
+while True:
+    try:
+        kw = int(input('Квартира:'))
+        break
+    except(ValueError):
+        print('Not a number')
+        
+print('\nВаш заказ принят')
+print(f'Наименование товара: {name}')
+print(f'Количество: {quantity}')
+print(f'Фамилия покупателя: {surname}')
+print(f'Контактный телефон: {phone}')
+print(f'Адрес:{index}, город {city}, улица {street}, дом {haus}, квартира {kw}')
