9ff5d170cf54dd8616b41eaf364dbf94b1473b1c
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-02-21 12:20:57 +0300

    Update con_t4.py

--- Conclusion/con_t4.py
+++ Conclusion/con_t4.py
@@ -6,3 +6,21 @@
 dict_a = {1:15, 2:21}
 dict_b = {3:13, 4:54}
 dict_c = {5:75, 6:68}
+
+
+dict_a = {1:15, 2:21}
+dict_b = {3:13, 4:54}
+dict_c = {5:75, 6:68}
+
+newd = {**dict_a,**dict_b,**dict_c}
+newdsrt = {}
+newdinvert = {}
+d = list(newd.values())
+d.sort()
+for i in d:
+    for key,value in newd.items():
+        if value == i:
+            newdsrt.update({key:value})
+            newdinvert.update({value:key})
+
+print(newd,newdsrt,newdinvert)
