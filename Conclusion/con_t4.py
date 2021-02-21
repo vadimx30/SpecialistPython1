1e982ebf1644391cd977eb569555aa972c820f0c
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-02-21 12:47:01 +0300

    Update con_t4.py

--- Conclusion/con_t4.py
+++ Conclusion/con_t4.py
@@ -6,3 +6,25 @@
 dict_a = {1:15, 2:21}
 dict_b = {3:13, 4:54}
 dict_c = {5:75, 6:68}
+
+dict_a.update(dict_b)
+dict_a.update(dict_c)
+print(dict_a)
+
+
+list_values = list(dict_a.values())
+list_values.sort()
+
+result = {}
+for i in list_values:
+    for key,value in dict_a.items():
+        if value == i:
+            result.update({key:value})
+print(result)
+
+result = {}
+for i in list_values:
+    for key,value in dict_a.items():
+        if value == i:
+            result.update({value:key})
+print(result)
