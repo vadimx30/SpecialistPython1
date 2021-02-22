fdb2ae9fcd86ff63945639a702ca17a855fa1fd5
dmitrylipat <77882257+dmitrylipat@users.noreply.github.com>
2021-02-22 10:23:27 +0300

    Update con_t3.py

--- Conclusion/con_t3.py
+++ Conclusion/con_t3.py
@@ -5,3 +5,31 @@
     Проверка работы функции выполняется через вторую строку.
 """
 code = '070411111426152419071413'
+
+import string
+code = '070411111426152419071413'
+code1 = list(code)
+a = list(string.ascii_lowercase)
+a.append(' ')
+b = []
+k = 0
+m = ''
+while k <= 26:
+    if k < 10:
+        m = '0' + str(k)
+        b.append(m)
+    else:
+        m = str(k)
+        b.append(m)
+    k += 1
+dicti = {}
+for i in range(27):
+    dicti[b[i]] = a[i]
+c = 0
+i = 0
+result = ''
+while i < len(code):
+    c = code[i] + code[i + 1]
+    i += 2
+    result += dicti[c]
+print(result)
